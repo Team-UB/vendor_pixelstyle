d60cee2326bdcee354267f8b6de69931f5b432b8
abun880007 <abun880007@msn.com>
2018-09-02 05:06:51 +0100

    remove bits no longer needed

--- config.mk
+++ config.mk
@@ -35,17 +35,6 @@ endif
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.wallpapers_loc_request_suw=true
 
-# Bootanimation
-ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_720.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1080.zip:system/media/bootanimation.zip
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/pixelstyle/media/bootanimation_1440.zip:system/media/bootanimation.zip
-else
-    $(error "PixelStyle: TARGET_BOOT_ANIMATION_RES is undefined")
-endif
-
 # Fonts
 PRODUCT_COPY_FILES += \
     vendor/pixelstyle/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
